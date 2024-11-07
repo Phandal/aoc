a6ded4fe2b46126c719dcc3593ba38cfdaffeb2b
Phandal <bfeld.111@gmail.com>
2024-11-07 16:57:19 -0600

    added some aoc functions

--- c/2015/include/aoc.h
+++ c/2015/include/aoc.h
@@ -1,6 +1,15 @@
 #ifndef AOC_H
 #define AOC_H
 
+#include <stdio.h>
+
+enum {
+  AOC_ERR = -1,
+  AOC_OK,
+};
+
+void aoc_fatalf(char *fmt, ...);
+int aoc_get_line(char line[], size_t max);
 char *aoc_hello(void);
 
 #endif // AOC_H
--- c/2015/lib/aoc.c
+++ c/2015/lib/aoc.c
@@ -1,3 +1,29 @@
 #include <aoc.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void aoc_fatalf(char *fmt, ...) {
+  va_list args;
+
+  va_start(args, fmt);
+  vfprintf(stderr, fmt, args);
+  va_end(args);
+
+  exit(1);
+}
+
+int aoc_get_line(char line[], size_t max) {
+  int c;
+  size_t i = 0;
+
+  while (i < max - 1 && (c = getchar()) != EOF && c != '\n') {
+    line[i++] = c;
+  }
+
+  line[i] = '\0';
+
+  return i;
+}
 
 char *aoc_hello(void) { return "Hello AOC!"; }
